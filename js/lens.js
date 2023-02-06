bd4b633063e5316a756496df1d437581c904c512
Lívia Gouvêa <livia@librecode.coop>
2023-02-06 15:11:38 -0300

    Utiliza ícone no lugar de "Fullscreen". Remove "Focus" Adiciona traget _blank em link de Orcid

--- js/lens.js
+++ js/lens.js
@@ -1378,6 +1378,7 @@
 					  i("a.orcid", {
 						href: "https://orcid.org/" + this.node.orcid,
 						text: "https://orcid.org/" + this.node.orcid,
+						target: '_blank',
 					  })
 					),
 				  this.node.present_address &&
@@ -4469,13 +4470,7 @@
 					e.appendChild(
 					  i("a.toggle.toggle-fullscreen", {
 						href: "#",
-						html: '<i class="fa fa-expand"></i> Fullscreen',
-					  })
-					),
-					e.appendChild(
-					  i("a.toggle-res.toggle.action-toggle-resource", {
-						href: "#",
-						html: '<i class="fa fa-eye"></i> Focus',
+						html: '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg>',
 					  })
 					),
 					t.appendChild(e),
